a377efb66865f6bd170eea1b34b1388e99a8b2c3
JHON JAIRO <jjmuesesq@unal.edu.co>
2018-09-30 03:07:40 -0500

    configurando link de navegación

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../resources/logo.svg';
 import '../styles/App.css';
+import {Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -9,10 +10,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Organización Estudiantes Universitarios Pastas Aldana</h1>
+          <Link to='/login'>Inciar Sesión</Link>
+          <Link to='/register'>Registrarse</Link>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
