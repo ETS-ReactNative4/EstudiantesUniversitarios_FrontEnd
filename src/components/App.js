60bae0e7dc7d67374f986653fb1335cf8bc73acf
JHON JAIRO <jjmuesesq@unal.edu.co>
2018-09-30 16:20:00 -0500

    implementando contenedor de contacto

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,7 @@ class App extends Component {
         <Link to='/register'>Registrarse</Link>
         </header>
 
+        {/*creando un componente carrucel de imagenes*/}
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
           <ol class="carousel-indicators">
@@ -93,6 +94,7 @@ class App extends Component {
 
         </div>
 
+        {/*creando contenedor texto centrado*/}
         <div class="container text-center">
           <h3>Organizacion Estrudiantes Universitarios</h3>
           <p><em>Conservando nuestra identidad cultural!</em></p>
@@ -137,6 +139,39 @@ class App extends Component {
               </div>
             </div>
 
+        </div>
+
+        {/*creando contenedor contacto*/}
+        <div class="container">
+          <h3 class="text-center">Contact</h3>
+          <p class="text-center"><em>We love our fans!</em></p>
+
+          <div class="row">
+            <div class="col-md-4">
+              <p>Fan? Drop a note.</p>
+              <p><span class="glyphicon glyphicon-map-marker"></span>Chicago, US</p>
+              <p><span class="glyphicon glyphicon-phone"></span>Phone: +00 1515151515</p>
+              <p><span class="glyphicon glyphicon-envelope"></span>Email: mail@mail.com</p>
+            </div>
+
+            <div class="col-md-8">
+              <div class="row">
+                <div class="col-sm-6 form-group">
+                  <input class="form-control" id="name" name="name" placeholder="Name" type="text" required/>
+                </div>
+                <div class="col-sm-6 form-group">
+                  <input class="form-control" id="email" name="email" placeholder="Email" type="email" required/>
+                </div>
+              </div>
+              <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea>
+              <br />
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <button class="btn pull-right" type="submit">Send</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
       </div>
