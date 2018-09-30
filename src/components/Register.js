efbf099b766b7bffd47b6baae760b7240bea82ef
JHON JAIRO <jjmuesesq@unal.edu.co>
2018-09-30 02:22:48 -0500

    configurando rutas de la aplicacion vistas login y register

---
+++ src/components/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class Register extends Component {
+  render() {
+    return (
+      <label>Register Creation page</label>
+    );
+  }
+}
+
+export default Register;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import Login from './components/Login';
+import Register from './components/Register';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+        <div>
+            <Route  path='/' component={App} />
+            <Route  path='/login' component={Login} />
+            <Route  path='/register' component={Register} />
+
+        </div>
+    </BrowserRouter>
+
+  , document.getElementById('root'));
 registerServiceWorker();
