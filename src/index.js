88078982b97ad7f3e4709601a383e46d08227ccc
JHON JAIRO <jjmuesesq@unal.edu.co>
2018-10-01 00:38:44 -0500

    actualizando rutas

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ ReactDOM.render(
   <BrowserRouter>
         <div>
             <Route  path='/' component={App} />
-            <Route  path='/login' exact component={Login} />
-            <Route  path='/register' exact component={Register} />
+            <Route  exact path='/login' exact component={Login} />
+            <Route  exact path='/register' exact component={Register} />
 
         </div>
     </BrowserRouter>
