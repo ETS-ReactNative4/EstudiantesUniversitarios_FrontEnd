520f8dfab71ef5854271ec51c567271a63eaaaba
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2018-10-17 16:39:20 -0500

    MODIFICACION ARCHIVOS
    se corrigen los archivos index.html y index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import {BrowserRouter, Route} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route exact path='/LoginPage' component={LoginPage} />
+            <Route exact path='/RegisterPage' component={RegisterPage} />
+        </div>
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
