47fd21a8e6ffed7ea124b43b3841524210442b3c
JHON JAIRO <jjmuesesq@unal.edu.co>
2018-10-24 16:54:04 -0500

    fusionando cambios de nuevo

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import HomePage from './HomePage';
 import CarouselImages from './CarouselImages';
 import Users from './Users'
 import '../styles/App.css';
 import ContactPage from "./ContactPage";
 
-=======
-import '../styles/App.css';
-import HomePage from './HomePage'
-import {Panel} from "react-bootstrap";
-import CarouselImages from "./CarouselImages";
->>>>>>> bc6f060ea1f918cb696f2385ec1299a9e82c2d1d
+
 
 class App extends Component {
 
@@ -20,33 +14,13 @@ class App extends Component {
         return (
           <div className="App ">
 
-<<<<<<< HEAD
               <HomePage/>
               <CarouselImages/>
-=======
-              <Panel className="Fondo">
-
-                  <Panel.Heading>
-                      <HomePage/>
-                  </Panel.Heading>
-
-                  <Panel.Body>
-                      <CarouselImages/>
-                  </Panel.Body>
-
-              </Panel>
->>>>>>> bc6f060ea1f918cb696f2385ec1299a9e82c2d1d
 
           </div>
 
         );
-<<<<<<< HEAD
   }
-=======
-  
-    }
-  
->>>>>>> bc6f060ea1f918cb696f2385ec1299a9e82c2d1d
 }
 
 export default App;
