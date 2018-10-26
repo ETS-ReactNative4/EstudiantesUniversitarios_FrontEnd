d2fd54ddd0f199e73017b898adef89461541367f
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2018-10-25 19:46:48 -0500

    Modificación registro e iniciar sesión

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import HomePage from './HomePage'
-import {Panel} from "react-bootstrap";
-import CarouselImages from "./CarouselImages";
+import RegisterPage from "./RegisterPage";
 
 class App extends Component {
 
     render() {
 
         return (
-          <div className="App ">
 
-              <Panel className="Fondo">
+          <div className="App">
 
-                  <Panel.Heading>
-                      <HomePage/>
-                  </Panel.Heading>
-
-                  <Panel.Body>
-                      <CarouselImages/>
-                  </Panel.Body>
-
-              </Panel>
+              <RegisterPage/>
 
           </div>
 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/HomePage.css';
 import {Breadcrumb, MenuItem, Nav, Navbar, NavDropdown, NavItem, PageHeader} from "react-bootstrap";
-import UserList from './UserList';
 import {Link} from 'react-router-dom';
+import CarouselImages from "./CarouselImages";
 
 
 class HomePage extends Component {
@@ -12,7 +12,7 @@ class HomePage extends Component {
 
           {/* ======================================================================== */}
           {/* BARRA PARA EL INICIO O REGISTRO DE USUARIO */}
-          <div >
+          <div>
               <Breadcrumb className="BarraHome">
                   <Breadcrumb.Item className="LetraColor" href="#">
                       Iniciar Sesión
@@ -40,39 +40,42 @@ class HomePage extends Component {
 
           {/* ======================================================================== */}
           {/* BARRA DE NAVEGACION DE LA PAGINA DE INICIO */}
-          <Navbar className="BarraNavegacion">
+
+          <Navbar className="BarraNavegacion" collapseOnSelect>
               <Navbar.Header>
                   <Navbar.Brand>
                       <a href="#">INICIO</a>
                   </Navbar.Brand>
                   <Navbar.Toggle />
               </Navbar.Header>
-              <Nav>
-                  <NavItem eventKey={1} href="#">
-                      Publicaciones
-                  </NavItem>
-                  <NavItem eventKey={2} href="#">
-                      Convenios Con Universidades
-                  </NavItem>
-                  <NavItem eventKey={3} href="#">
-                      Eventos
-                  </NavItem>
-              </Nav>
-
-              <Nav pullRight >
-                  <NavDropdown eventKey={1} title="Información" id="basic-nav-dropdown">
-                      <MenuItem eventKey={1.1}>¿Quienes somos?</MenuItem>
-                      <MenuItem eventKey={1.2}>Misión</MenuItem>
-                      <MenuItem eventKey={1.3}>Visión</MenuItem>
-                  </NavDropdown>
-                  <NavItem className="MenuIzquierda" eventKey={2} href="#">
-                      Contactenos
-                  </NavItem>
-              </Nav>
+              <Navbar.Collapse>
+                  <Nav>
+                      <NavItem eventKey={1} href="#">
+                          /Publicaciones/
+                      </NavItem>
+                      <NavItem eventKey={2} href="#">
+                          /Convenios Con Universidades/
+                      </NavItem>
+                      <NavItem eventKey={3} href="#">
+                          /Eventos/
+                      </NavItem>
+                  </Nav>
+                  <Nav pullRight>
+                      <NavDropdown eventKey={1} title="Información" id="basic-nav-dropdown">
+                          <MenuItem eventKey={1.1}>¿Quienes somos?</MenuItem>
+                          <MenuItem eventKey={1.2}>Misión</MenuItem>
+                          <MenuItem eventKey={1.3}>Visión</MenuItem>
+                      </NavDropdown>
+                      <NavItem className="MenuIzquierda" eventKey={2} href="#">
+                          Contactenos
+                      </NavItem>
+                  </Nav>
+              </Navbar.Collapse>
           </Navbar>
+
+          <CarouselImages/>
+
           {/* ======================================================================== */}
-
-
 
       </div>
     );
