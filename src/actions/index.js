17d0203dc31de0b6aa29b451c4885e11f53a74e7
Jhon Jairo Mueses Quitiaquez <jjmuesesq@unal.edu.co>
2018-10-28 23:18:30 -0500

    actualizando directorio reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
 // src / actions / index.js
 import { ADD_ARTICLE } from "../constants/action-types";
 
-export const addArticle = article = > ( { type : "ADD_ARTICLE" , payload : article } ) ;
+export const addArticle = article => ( { type : "ADD_ARTICLE" , payload : article } ) ;
