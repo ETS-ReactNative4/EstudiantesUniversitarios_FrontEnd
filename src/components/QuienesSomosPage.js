480b5680f23b2464231cb13ac125c13bd030088a
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2018-10-28 23:40:56 -0500

    Arreglo de estilos

--- src/components/QuienesSomosPage.js
+++ src/components/QuienesSomosPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../styles/MisionPage.css';
+import '../styles/QuienesSomosPage.css';
 import BarHome from "./BarHome";
 import {Grid, Jumbotron} from "react-bootstrap";
 
@@ -10,38 +10,35 @@ class MisionPage extends Component {
 
         return (
 
-          <div className="MisionPage container">
+          <div className="MisionPage ">
 
-              <BarHome/>
+              <div className={"container"}>
 
-              <Grid>
-                  <Jumbotron >
-                      <h1 className="TituloMision">Misión</h1>
+                  <BarHome/>
 
-                      <br/>
-                      <br/>
+                  <Grid>
+                      <Jumbotron >
 
-                      <div className="CentrarTexto">
+                          <div className="CentrarTexto">
 
-                          <br/>
-                          <br/>
+                              <div>
 
-                          <div>
+                                  <h1>Organizacion Estrudiantes Universitarios</h1>
+                                  <br/>
+                                  <h2>Conservando nuestra identidad cultural!</h2>
+                                  <br/>
+                                  <p>La organización de estudiantes indígenas universitarios va encaminada a la integración de indígenas estudiantes regulares, egresados y titulados de las diferentes Universidades públicas o privadas del país, con el propósito de promover la recuperación, protección y conservación de la identidad cultural, por medio de mingas de trabajo, mingas de pensamiento, tulpas donde compartamos los saberes que nuestros mayores nos dan como herencia y promoverlos de generación en generación, así mismo luchar por los derechos, beneficios y oportunidades de progreso que como jóvenes indígenas poseemos</p>
 
-                              <p>Integración de Estudiantes Indigenas Universitarios.</p>
-                              <br/>
-                              <br/>
-                              <p>Promover la recuperación, protección y conservación de la identidad cultural.</p>
-                              <br/>
-                              <br/>
-                              <p>luchar por los derechos, beneficios y oportunidades de progreso que como jóvenes indígenas poseemos.</p>
+                              </div>
 
                           </div>
 
-                      </div>
+                      </Jumbotron>
 
-                  </Jumbotron>
-              </Grid>
+                  </Grid>
+
+              </div>
+
           </div>
 
         );
