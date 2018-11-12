8c668dd9594cb3ee3f3dab2553453648cc575a6c
Jhon Jairo Mueses Quitiaquez <jjmuesesq@unal.edu.co>
2018-11-12 12:37:45 -0500

    configurando LoginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,6 +5,30 @@ import {Button, ButtonGroup, Col, ControlLabel, Form, FormControl, FormGroup, Ju
 
 class LoginPage extends Component {
 
+    handleSubmit = event => {
+        event.preventDefault();
+
+        fetch("http://unipastas-back.herokuapp.com/user_token",
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    auth: {
+                        email: this.inputNode1.value,
+                        password: this.inputNode2.value
+                    }
+                }),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            },
+        ).then(res => res.json()).then(res => (console.log(res.jwt),
+                window.localStorage.setItem('jwt', res.jwt)
+        ))
+            .then(() => this.props.history.push('/welcome'))
+            .catch(function(error){console.log('Error: ', error.message)})
+    }
+
     render() {
 
         return (
@@ -33,30 +57,32 @@ class LoginPage extends Component {
                                 <br/>
                                 <br/>
 
-                                <Form horizontal>
+
+                                <Form horizontal onSubmit={this.handleSubmit}>
                                     <FormGroup controlId="formHorizontalEmail">
                                         <Col componentClass={ControlLabel} sm={3}>
-                                            E-Mail:
+                                            <label htmlFor='email'>E-mail:</label> <br/>
                                         </Col>
 
                                         <Col sm={9}>
-                                            <FormControl type="email" placeholder="ejemplo@prueba.com" />
+                                            <FormControl type="email" placeholder="ejemplo@prueba.com" id="email" name="email" ref={node => {this.inputNode1 = node}}/>
+
                                         </Col>
                                     </FormGroup>
 
                                     <FormGroup controlId="formHorizontalPassword">
                                         <Col componentClass={ControlLabel} sm={3}>
-                                            Contraseña:
+                                            <label htmlFor='password'>password</label> <br/>
                                         </Col>
                                         <Col sm={9}>
-                                            <FormControl type="password" placeholder="contraseña" />
+                                            <FormControl type="password" placeholder="contraseña" id='password' name='password' ref={node => {this.inputNode2 = node}}/>
                                         </Col>
                                     </FormGroup>
 
                                     <FormGroup className="BarraBotones">
 
                                         <ButtonGroup  >
-                                            <Button type="submit" bsStyle="success"> Entrar </Button>
+                                            <Button type="submit" bsStyle="success"> Ingresar </Button>
                                             <Button bsStyle="danger">
                                                 <Link to='/HomePage'>Volver</Link>
                                             </Button>
