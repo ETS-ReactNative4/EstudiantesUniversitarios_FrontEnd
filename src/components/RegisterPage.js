30ff01c3574a4a7b986104222c2f06952714dbe8
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2018-12-15 11:29:41 -0500

    validación campos registro

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -5,12 +5,6 @@ import {Link} from "react-router-dom";
 
 class RegisterPage extends Component {
 
-     // validarNombre(event) {
-    //     const soloTexto = /^([A-Za-z])+((\s)*([A-Za-z]))*$/;
-    //     const validName = soloTexto.test(event.target.value);
-    //     console.log("nombre --> " + this.state.name);
-    // }
-
     constructor(props) {
         super(props);
         
@@ -27,10 +21,11 @@ class RegisterPage extends Component {
 
         this.validateName = this.validateName.bind(this);
         this.validateIdNumber =  this.validateIdNumber.bind(this);
+        this.validateEmail = this.validateEmail.bind(this);
+        this.validatePassword = this.validatePassword.bind(this);
     }
 
     validateName(event) {
-        // console.log(event.target.value);
         const soloTexto = /^([A-Za-z])+((\s)*([A-Za-z]))*$/;
         const name = event.target.value;
         const isValidName = soloTexto.test(name);
@@ -39,12 +34,28 @@ class RegisterPage extends Component {
     }
 
     validateIdNumber(event) {
-        const soloNumeros = /^([A-Za-z])+((\s)*([A-Za-z]))*$/;
+        const soloNumeros = /^([0-9])+$/;
         const idNumber = event.target.value;
         const isValidIdNumber = soloNumeros.test(idNumber);
         this.setState({ isValidIdNumber });
         this.setState({ idNumber });
     }
+
+    validateEmail(event) {
+        const soloEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        const email = event.target.value;
+        const isValidEmail = soloEmail.test(email);
+        this.setState({ isValidEmail });
+        this.setState({ email });
+    }
+
+    validatePassword(event) {
+        const soloPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/;
+        const password = event.target.value;
+        const isValidPassword = soloPassword.test(password);
+        this.setState({ isValidPassword });
+        this.setState({ password });
+    } 
 
     render() {
 
@@ -93,14 +104,14 @@ class RegisterPage extends Component {
                                             
                                         </Col>
                                         <Col xs={12} md={9}>
-                                            <HelpBlock>{(this.state.name.length === 0  & !this.state.isValidName) || (this.state.name.length > 1  & !this.state.isValidName) ? "El nombre no puede contener numeros ni ser vacio" : ""}</HelpBlock>
+                                            <HelpBlock>{ (this.state.name.length === 0  & !this.state.isValidName) || (this.state.name.length > 1  & !this.state.isValidName) ? "El nombre no puede contener numeros ni ser vacio" : ""}</HelpBlock>
                                         </Col>
                                     </Row> 
                                     <FormControl.Feedback />             
                                 </FormGroup>
 
 
-                                <FormGroup controlId="formValidationIdNumber" validationState={this.state.idNumber.length === 1 ? null : this.state.validIdNumber ? "success" : "error"}>
+                                <FormGroup controlId="formValidationIdNumber" validationState={ this.state.idNumber.length === 1 ? null : this.state.isValidIdNumber ? "success" : "error"}>
                                     <Row>
                                         <Col xs={12} md={3}>
                                             <ControlLabel>NÚMERO DE IDENTIFICACIÓN: </ControlLabel>
@@ -114,20 +125,20 @@ class RegisterPage extends Component {
                                                 
                                         </Col>
                                         <Col xs={12} md={9}>
-                                            <HelpBlock>{(this.state.idNumber.length === 0  & !this.state.validIdNumber) || (this.state.name.length > 1  & !this.state.isValidName)? "El número de identificación solo pude contener números" : ""}</HelpBlock>   
+                                            <HelpBlock>{ (this.state.idNumber.length === 0  & !this.state.isValidIdNumber) || (this.state.idNumber.length > 1  & !this.state.isValidIdNumber) ? "El número de identificación solo debe contener números y no puede ser vacio" : ""}</HelpBlock>
                                         </Col>
                                     </Row>
                                     <FormControl.Feedback />
                                 </FormGroup>
 
 
-                                <FormGroup controlId="formValidationEmail" validationState={this.state.email.length === 1 ? null : this.state.validEmail ? "success" : "error"}>
+                                <FormGroup controlId="formValidationEmail" validationState={ this.state.email.length === 1 ? null : this.state.isValidEmail ? "success" : "error"}>
                                     <Row>
                                         <Col xs={12} md={3}>
                                             <ControlLabel>CORREO ELECTRÓNICO:</ControlLabel>
                                         </Col>
                                         <Col xs={12} md={9}>
-                                            <FormControl type="text" />
+                                            <FormControl type="text" onChange={ this.validateEmail }/>
                                             
                                         </Col>
                                     </Row>
@@ -136,19 +147,19 @@ class RegisterPage extends Component {
                                                 
                                         </Col>
                                         <Col xs={12} md={9}>
-                                            <HelpBlock>{this.state.email.length === 0  & !this.state.validEmail ? "Por favor ingrese un corre electronico válido" : ""}</HelpBlock>    
+                                            <HelpBlock>{ this.state.email.length === 0  & !this.state.isValidEmail ? "Por favor ingrese un corre electronico válido" : ""}</HelpBlock>    
                                         </Col>
                                     </Row>
                                     <FormControl.Feedback />
                                 </FormGroup>
 
-                                <FormGroup controlId="formValidationPassword" validationState={this.state.password.length === 1 ? null : this.state.validPassword ? "success" : "error"}>
+                                <FormGroup controlId="formValidationPassword" validationState={ this.state.password.length === 1 ? null : this.state.isValidPassword ? "success" : "error"}>
                                     <Row>
                                         <Col xs={12} md={3}>
                                             <ControlLabel>CONTRASEÑA:</ControlLabel>                                        
                                         </Col>
                                         <Col xs={12} md={9}>
-                                            <FormControl type="text" />
+                                            <FormControl type="password" onChange={ this.validatePassword }/>
                                             
                                         </Col>
                                     </Row>
@@ -157,7 +168,7 @@ class RegisterPage extends Component {
                                                 
                                         </Col>
                                         <Col xs={12} md={9}>                                                    
-                                            <HelpBlock>{this.state.password.length === 0  & !this.state.validPassword ? "La contraseña debe contener por lo menos 6 caracteres" : ""}</HelpBlock>                                            
+                                            <HelpBlock>{this.state.password.length === 0  & !this.state.isValidPassword ? "La contraseña debe contener por lo menos 6 caracteres" : ""}</HelpBlock>                                            
                                         </Col>
                                     </Row>
                                     <FormControl.Feedback />
@@ -167,9 +178,6 @@ class RegisterPage extends Component {
 
                                 <Form horizontal>
                         
-
-                                    <p>{JSON.stringify(this.state)}</p>
-
                                     <FormGroup className="BarraBotones">
 
                                         <ButtonGroup  >
