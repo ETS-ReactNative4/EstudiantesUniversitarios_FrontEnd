dd9e3f7948987c263dc49e647860707f64197550
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2018-12-15 14:45:24 -0500

    corrección de estilos css

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/RegisterPage.css';
-import {HelpBlock, ControlLabel, Button, ButtonGroup, Col, Form, FormControl, FormGroup, Jumbotron, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import {HelpBlock, ControlLabel, Button, ButtonGroup, Col, Form, FormControl, FormGroup, Jumbotron, Row} from "react-bootstrap";
 
 class RegisterPage extends Component {
 
@@ -59,125 +59,114 @@ class RegisterPage extends Component {
 
     render() {
 
-
         return (
 
             <div className="RegisterPage">
 
-                <div className={"container"}>
-
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
+                <div className="container">
                    
-
                     <Row>
 
-                        <Col xs={12} md={2}>
-                        </Col>
+                        <Col xs={12} md={2} />
 
                         <Col xs={12} md={8}>
 
-                            <Jumbotron className="Ventana">
+                            <Jumbotron className="VentanaRegister">
 
                                 <div className="LabelsRegister">
                                         <h1 > Registrarse </h1>
                                 </div>
                                 <br></br>
 
-                                {/* ************************************************************************************ */}
-                                {/* ************************************************************************************ */}
-                                <FormGroup controlId="formValidationName" validationState={this.state.name.length === 1 ? null : this.state.isValidName ? "success" : "error"}>
-                                    <Row>
-                                        <Col xs={12} md={3}>
-                                            <ControlLabel>NOMBRE:</ControlLabel>
-                                        </Col>
-                                        <Col xs={12} md={9}>
-                                                <FormControl type="text" onChange={this.validateName}/>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col xs={12} md={3}>
-                                            
-                                        </Col>
-                                        <Col xs={12} md={9}>
-                                            <HelpBlock>{ (this.state.name.length === 0  & !this.state.isValidName) || (this.state.name.length > 1  & !this.state.isValidName) ? "El nombre no puede contener numeros ni ser vacio" : ""}</HelpBlock>
-                                        </Col>
-                                    </Row> 
-                                    <FormControl.Feedback />             
-                                </FormGroup>
-
-
-                                <FormGroup controlId="formValidationIdNumber" validationState={ this.state.idNumber.length === 1 ? null : this.state.isValidIdNumber ? "success" : "error"}>
-                                    <Row>
-                                        <Col xs={12} md={3}>
-                                            <ControlLabel>NÚMERO DE IDENTIFICACIÓN: </ControlLabel>
-                                        </Col>
-                                        <Col xs={12} md={9}>
-                                            <FormControl type="text" onChange={this.validateIdNumber}/>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col xs={12} md={3}>
-                                                
-                                        </Col>
-                                        <Col xs={12} md={9}>
-                                            <HelpBlock>{ (this.state.idNumber.length === 0  & !this.state.isValidIdNumber) || (this.state.idNumber.length > 1  & !this.state.isValidIdNumber) ? "El número de identificación solo debe contener números y no puede ser vacio" : ""}</HelpBlock>
-                                        </Col>
-                                    </Row>
-                                    <FormControl.Feedback />
-                                </FormGroup>
-
-
-                                <FormGroup controlId="formValidationEmail" validationState={ this.state.email.length === 1 ? null : this.state.isValidEmail ? "success" : "error"}>
-                                    <Row>
-                                        <Col xs={12} md={3}>
-                                            <ControlLabel>CORREO ELECTRÓNICO:</ControlLabel>
-                                        </Col>
-                                        <Col xs={12} md={9}>
-                                            <FormControl type="text" onChange={ this.validateEmail }/>
-                                            
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col xs={12} md={3}>
-                                                
-                                        </Col>
-                                        <Col xs={12} md={9}>
-                                            <HelpBlock>{ this.state.email.length === 0  & !this.state.isValidEmail ? "Por favor ingrese un corre electronico válido" : ""}</HelpBlock>    
-                                        </Col>
-                                    </Row>
-                                    <FormControl.Feedback />
-                                </FormGroup>
-
-                                <FormGroup controlId="formValidationPassword" validationState={ this.state.password.length === 1 ? null : this.state.isValidPassword ? "success" : "error"}>
-                                    <Row>
-                                        <Col xs={12} md={3}>
-                                            <ControlLabel>CONTRASEÑA:</ControlLabel>                                        
-                                        </Col>
-                                        <Col xs={12} md={9}>
-                                            <FormControl type="password" onChange={ this.validatePassword }/>
-                                            
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col xs={12} md={3}>
-                                                
-                                        </Col>
-                                        <Col xs={12} md={9}>                                                    
-                                            <HelpBlock>{this.state.password.length === 0  & !this.state.isValidPassword ? "La contraseña debe contener por lo menos 6 caracteres" : ""}</HelpBlock>                                            
-                                        </Col>
-                                    </Row>
-                                    <FormControl.Feedback />
-                                </FormGroup>
-                                {/* ************************************************************************************ */}
-                                {/* ************************************************************************************ */}
-
                                 <Form horizontal>
-                        
+                                    {/* ************************************************************************************ */}
+                                    {/* ************************************************************************************ */}
+                                    <FormGroup controlId="formValidationName" validationState={this.state.name.length === 1 ? null : this.state.isValidName ? "success" : "error"}>
+                                        <Row>
+                                            <Col className="MiniLabelsRegister" xs={12} md={3}>
+                                                <ControlLabel >NOMBRE:</ControlLabel>
+                                            </Col>
+                                            <Col xs={12} md={9}>
+                                                    <FormControl type="text" onChange={this.validateName}/>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col xs={12} md={3}>
+                                                
+                                            </Col>
+                                            <Col xs={12} md={9}>
+                                                <HelpBlock>{ (this.state.name.length === 0  & !this.state.isValidName) || (this.state.name.length > 1  & !this.state.isValidName) ? "El nombre no puede contener numeros ni ser vacio" : ""}</HelpBlock>
+                                            </Col>
+                                        </Row> 
+                                        <FormControl.Feedback />             
+                                    </FormGroup>
+
+
+                                    <FormGroup controlId="formValidationIdNumber" validationState={ this.state.idNumber.length === 1 ? null : this.state.isValidIdNumber ? "success" : "error"}>
+                                        <Row>
+                                            <Col className="MiniLabelsRegister" xs={12} md={3}>
+                                                <ControlLabel>NÚMERO DE IDENTIFICACIÓN: </ControlLabel>
+                                            </Col>
+                                            <Col xs={12} md={9}>
+                                                <FormControl type="text" onChange={this.validateIdNumber}/>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col xs={12} md={3}>
+                                                    
+                                            </Col>
+                                            <Col xs={12} md={9}>
+                                                <HelpBlock>{ (this.state.idNumber.length === 0  & !this.state.isValidIdNumber) || (this.state.idNumber.length > 1  & !this.state.isValidIdNumber) ? "El número de identificación solo debe contener números y no puede ser vacio" : ""}</HelpBlock>
+                                            </Col>
+                                        </Row>
+                                        <FormControl.Feedback />
+                                    </FormGroup>
+
+
+                                    <FormGroup controlId="formValidationEmail" validationState={ this.state.email.length === 1 ? null : this.state.isValidEmail ? "success" : "error"}>
+                                        <Row>
+                                            <Col className="MiniLabelsRegister" xs={12} md={3}>
+                                                <ControlLabel>CORREO ELECTRÓNICO:</ControlLabel>
+                                            </Col>
+                                            <Col xs={12} md={9}>
+                                                <FormControl type="text" onChange={ this.validateEmail }/>
+                                                
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col xs={12} md={3}>
+                                                    
+                                            </Col>
+                                            <Col xs={12} md={9}>
+                                                <HelpBlock>{ (this.state.email.length === 0  & !this.state.isValidEmail) || (this.state.email.length > 1  & !this.state.isValidEmail) ? "Por favor ingrese un corre electronico válido" : ""}</HelpBlock>    
+                                            </Col>
+                                        </Row>
+                                        <FormControl.Feedback />
+                                    </FormGroup>
+
+                                    <FormGroup controlId="formValidationPassword" validationState={ this.state.password.length === 1 ? null : this.state.isValidPassword ? "success" : "error"}>
+                                        <Row>
+                                            <Col className="MiniLabelsRegister" xs={12} md={3}>
+                                                <ControlLabel>CONTRASEÑA:</ControlLabel>                                        
+                                            </Col>
+                                            <Col xs={12} md={9}>
+                                                <FormControl type="password" onChange={ this.validatePassword }/>
+                                                
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col xs={12} md={3}>
+                                                    
+                                            </Col>
+                                            <Col xs={12} md={9}>                                                    
+                                                <HelpBlock>{ (this.state.password.length === 0  & !this.state.isValidPassword) || (this.state.password.length > 1  & !this.state.isValidPassword) ? "La contraseña debe contener por lo menos 6 caracteres" : ""}</HelpBlock>                                            
+                                            </Col>
+                                        </Row>
+                                        <FormControl.Feedback />
+                                    </FormGroup>
+                                    {/* ************************************************************************************ */}
+                                    {/* ************************************************************************************ */}
+
                                     <FormGroup className="BarraBotones">
 
                                         <ButtonGroup  >
@@ -190,16 +179,13 @@ class RegisterPage extends Component {
                                             </Button>
                                         </ButtonGroup>
 
-                                    </FormGroup>
+                                </FormGroup>
                                 </Form>
-
                             </Jumbotron>
-
                         </Col>
                     </Row>
 
-                    <Col xs={12} md={2}>
-                    </Col>
+                    <Col xs={12} md={2} />
                 </div>
             </div>
 
